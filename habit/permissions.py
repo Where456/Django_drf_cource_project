20643c53109810268d56cd431655d1848b66b7bf
Where456 <daanagrigoreva1@gmail.com>
2024-04-04 10:05:19 +0300

    Initial commit

---
+++ habit/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    message = 'Доступ ограничен! Вы не являетесь владельцем записи'
+
+    class IsOwner(permissions.BasePermission):
+        """ Проверка прав автора публикации"""
+
+        def has_permission(self, request, view):
+            """Автор публикации может редактировать и удалять только свои объекты"""
+            if request.user == view.get_object().user:
+                return request.method in ['PUT', 'PATCH', 'DELETE']
+
+            return False
